d0031abe4368b641476933f943ccc173be4fcd66
Jia Jia <angersax@sina.com>
2024-02-13 11:38:46 +0800

    chore(gh): add support for agent in actions

--- script/build.sh
+++ script/build.sh
@@ -5,4 +5,4 @@ ldflags="-s -w"
 go env -w GOPROXY=https://goproxy.cn,direct
 
 CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=$(go env GOOS) go build -ldflags "$ldflags" -o bin/agent agent/agent.go
-CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=$(go env GOOS) go build -ldflags "$ldflags" -o bin/insight example/main.go
+CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=$(go env GOOS) go build -ldflags "$ldflags" -o bin/insight example/example.go
