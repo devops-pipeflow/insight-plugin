be46bf3d1c2c669d2ba5ac481e25fdf1dbd98f8d
Jia Jia <angersax@sina.com>
2024-02-16 23:49:34 +0800

    feat(agent): add support for agent deployment

--- agent/agent.sh
+++ agent/agent.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
+# NOTES
+# checksum=$(shasum -a 256 /path/to/agent) sed -i "s:^CHECKSUMS=$:CHECKSUMS=\"$checksum\":g" agent.sh && ./agent.sh
+
 # Generate checksums
-# TBD: FIXME
+CHECKSUMS=
 
 # Verify checksums
-# TBD: FIXME
+echo "$CHECKSUMS" | shasum -a 256 -c -s
+ret=$?
+if [ $ret != 0 ]; then
+  echo 'Invalid checksums'
+  exit 1
+fi
 
 # Deploy agent
 # TBD: FIXME
+
+exit 0
