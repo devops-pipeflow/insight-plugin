bc9e95350b49c52e62e2e142ba61c2416a553003
Jia <49358172+craftslab@users.noreply.github.com>
2024-02-18 18:55:21 +0800

    feat(agent): update script for checksum

--- script/build.sh
+++ script/build.sh
@@ -6,3 +6,8 @@ go env -w GOPROXY=https://goproxy.cn,direct
 
 CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=$(go env GOOS) go build -ldflags "$ldflags" -o bin/agent agent/agent.go
 CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=$(go env GOOS) go build -ldflags "$ldflags" -o bin/insight example/example.go
+
+pushd bin || exit
+checksum=$(shasum -a 256 agent); sed "s:^CHECKSUM=$:CHECKSUM=\"$checksum\":g" ../agent/agent.sh > agent.sh
+chmod +x agent.sh
+popd || exit
