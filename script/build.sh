9a91bad53b3eb2352d3e4a27958087e097e850d6
Jia <49358172+craftslab@users.noreply.github.com>
2024-03-05 09:11:39 +0000

    fix(build): fix incorrect build target

--- script/build.sh
+++ script/build.sh
@@ -9,7 +9,7 @@ if [ "$os" = "windows" ]; then
   CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=windows go build -ldflags "$ldflags" -o bin/insight example/example.go
 else
   CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=linux go build -ldflags "$ldflags" -o bin/agent agent/agent.go
-  CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=linux go build -ldflags "$ldflags" -o bin/insight example/example.go
+  CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=linux go build -ldflags "$ldflags" -o bin/insight .
   pushd bin || exit
   checksum=$(shasum -a 256 agent); sed "s:^CHECKSUM=$:CHECKSUM=\"$checksum\":g" ../agent/agent.sh > agent.sh
   chmod +x agent.sh
