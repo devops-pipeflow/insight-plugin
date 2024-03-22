ce32181b83fdd6b20e770e093f046c5b5bd94a92
Jia <49358172+craftslab@users.noreply.github.com>
2024-03-22 11:49:58 +0800

    chore(script): reduce the file size of agent with upx

--- script/build.sh
+++ script/build.sh
@@ -11,6 +11,7 @@ else
   CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=linux go build -ldflags "$ldflags" -o bin/agent agent/agent.go
   CGO_ENABLED=0 GOARCH=$(go env GOARCH) GOOS=linux go build -ldflags "$ldflags" -o bin/insight .
   pushd bin || exit
+  upx agent
   checksum=$(shasum -a 256 agent); sed "s:^CHECKSUM=$:CHECKSUM=\"$checksum\":g" ../agent/agent.sh > agent.sh
   chmod +x agent.sh
   popd || exit
